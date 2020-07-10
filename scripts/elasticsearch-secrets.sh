813a9bb22218a3c3f69bcd2b848c2094601129ed
Zhenfang Wei <kopkop@gmail.com>
2020-07-10 23:34:59 +0200

    Minor update

--- scripts/elasticsearch-secrets.sh
+++ scripts/elasticsearch-secrets.sh
@@ -3,7 +3,7 @@ docker rm -f elastic-helm-charts-certs || true
 rm -f elastic-certificates.p12 elastic-certificate.pem elastic-stack-ca.p12 || true
 password=$([ ! -z "$ELASTIC_PASSWORD" ] && echo $ELASTIC_PASSWORD || echo $(docker run --rm busybox:1.31.1 /bin/sh -c "< /dev/urandom tr -cd '[:alnum:]' | head -c20")) && \
 docker run --name elastic-helm-charts-certs -i -w /app \
-	docker.elastic.co/elasticsearch/elasticsearch:7.7.1 \
+	docker.elastic.co/elasticsearch/elasticsearch:7.8.0 \
 	/bin/sh -c " \
 		elasticsearch-certutil ca --out /app/elastic-stack-ca.p12 --pass '' && \
 		elasticsearch-certutil cert --name security-master --dns security-master --ca /app/elastic-stack-ca.p12 --pass '' --ca-pass '' --out /app/elastic-certificates.p12" && \
